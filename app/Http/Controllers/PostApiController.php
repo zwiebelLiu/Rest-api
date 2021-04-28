d5f3016696b812695a45316edebd93cfc3827f2c
zwiebelLiu <qin.liu1989@yahoo.de>
2021-04-28 23:07:26 +0200

    creat DB rest-api router api.php creat updet get delte PostApiController --resource

---
+++ app/Http/Controllers/PostApiController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Post[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        return  Post::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+                                'title'=>'required',
+                                'content'=>'required',
+                            ]);
+        return Post::create([
+                                'title'=>request('title'),
+                                'content'=>request('content'),
+                            ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post=Post::findOrFail($id);
+        $post->update([
+                          'title'=>request('title'),
+                          'content'=>request('content'),
+                      ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post=Post::findOrFail($id);
+        $success =$post->delete();
+        return ['success' => $success];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostApiController;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,40 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Route::get('/posts',[PostApiController::class,index]);
+Route::resource('posts', PostApiController::class);
+/*
+
+Route::post('/posts',function ()
+{
+    request()->validate([
+        'title'=>'required',
+        'content'=>'required',
+                        ]);
+    return Post::create([
+        'title'=>request('title'),
+        'content'=>request('content'),
+                        ]);
+});
+*/
+/*
+Route::put('/posts/{id}',function ($id){
+    $post=Post::findOrFail($id);
+
+});
+*/
+/*
+Route::put('/posts/{post}',function (Post $post){
+   $post->update([
+                     'title'=>request('title'),
+                     'content'=>request('content'),
+                 ]);
+    });
+
+Route::delete('/posts/{post}',function (Post $post){
+    $success =$post->delete();
+    return ['success' => $success];
+});
+*/
